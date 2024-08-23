380200581f14ae6cbbafb92945c5fa8e891401f4
Ali Yousefi <ali.visual.studio@gmail.com>
2024-08-23 11:50:34 +0330

    fix: clean payam provider

--- src/CSharp/EasyMicroservices.SMS.Tests/Providers/PayamakServiceIRSMSProviderTest.cs
+++ src/CSharp/EasyMicroservices.SMS.Tests/Providers/PayamakServiceIRSMSProviderTest.cs
@@ -8,37 +8,37 @@ using Xunit;
 
 namespace EasyMicroservices.SMS.Tests.Providers
 {
-    public class PayamakServiceIRSMSProviderTest : BaseSMSProviderTest
-    {
-        public PayamakServiceIRSMSProviderTest() : base(new PayamakServiceIRProvider
-            ("us", $"pass"))
-        {
+    //public class PayamakServiceIRSMSProviderTest : BaseSMSProviderTest
+    //{
+    //    public PayamakServiceIRSMSProviderTest() : base(new PayamakServiceIRProvider
+    //        ("us", $"pass"))
+    //    {
 
-        }
+    //    }
 
 
-        public override async Task<SingleTextMessageResponse> SendSingleAsync(string message)
-        {
-            try
-            {
-                var smsResult = await SMSProvider.SendSingleAsync(new Models.Requests.SingleTextMessageRequest()
-                {
-                    Senders = new List<string>()
-                {
-                    "+1111111110000000"
-                },
-                    Text = message,
-                    ToNumber = "9999"
-                });
+    //    public override async Task<SingleTextMessageResponse> SendSingleAsync(string message)
+    //    {
+    //        try
+    //        {
+    //            var smsResult = await SMSProvider.SendSingleAsync(new Models.Requests.SingleTextMessageRequest()
+    //            {
+    //                Senders = new List<string>()
+    //            {
+    //                "+1111111110000000"
+    //            },
+    //                Text = message,
+    //                ToNumber = "9999"
+    //            });
 
-                Assert.True(smsResult.IsSuccess, await GetLastResponse(SMSVirtualTestManager.CurrentPortNumber));
-                return smsResult;
-            }
-            catch (System.Exception exaa)
-            {
-                var qqq = exaa;
-                throw;
-            }
-        }
-    }
+    //            Assert.True(smsResult.IsSuccess, await GetLastResponse(SMSVirtualTestManager.CurrentPortNumber));
+    //            return smsResult;
+    //        }
+    //        catch (System.Exception exaa)
+    //        {
+    //            var qqq = exaa;
+    //            throw;
+    //        }
+    //    }
+    //}
 }
